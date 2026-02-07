b0338374e6681163193c544c0ec3bee98246ed78
karthik-cyberexpert <itzkarthik.cyber@gmail.com>
2026-02-07 11:22:59 +0530

    Added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.adhiyamaan.broucher',
   appName: 'Adhiyamaan Info',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'http://192.168.46.151:5173',
+    cleartext: true
+  }
 };
 
 export default config;
